7e9f177b78608d81f0458cf1f79d58857df102a0
Chris Brandhorst <chris.brandhorst@topicus.nl>
2016-04-30 16:41:03 +0200

    Can receive messages, parsing a few

---
+++ examples/remote.js
@@ -0,0 +1,18 @@
+"use strict"
+
+const HdmiCecController = require('../lib/hdmi-cec-control')
+
+var controller = new HdmiCecController({
+  clientPath:   '/usr/osmc/bin/cec-client',
+  remote:       {
+    user:       'osmc',
+    host:       'osmc',
+    password:   'osmc'
+  }
+})
+
+controller.connect()
+
+controller.on('error', (err, resp) => {
+  // console.error(err + ": " + resp)
+})
---
+++ index.js
@@ -0,0 +1,1 @@
+module.exports = require('./lib/hdmi-cec-control');
