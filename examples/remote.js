af4f95b341d6d429c51e4a0722e1bf631f45f385
Chris Brandhorst <chris.brandhorst@topicus.nl>
2016-05-28 18:44:43 +0200

    Moar work

--- examples/remote.js
+++ examples/remote.js
@@ -3,7 +3,7 @@
 const HdmiCecController = require('../lib/hdmi-cec-control')
 
 var controller = new HdmiCecController({
-  clientPath:   '/usr/osmc/bin/cec-client',
+  clientPath:   '/usr/osmc/bin/cec-client --type r --osd-name Smartie',
   remote:       {
     user:       'osmc',
     host:       'osmc',
