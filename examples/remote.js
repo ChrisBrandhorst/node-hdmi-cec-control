2cf4fbbfefab989538a08ba4c10df37582cf87e8
Chris Brandhorst <chris.brandhorst@topicus.nl>
2017-02-18 21:38:05 +0100

    Small improvements

--- examples/remote.js
+++ examples/remote.js
@@ -1,6 +1,7 @@
 'use strict'
 
 const HdmiCecController = require('../lib/hdmi-cec-control')
+const util = require('util')
 
 var controller = global.controller = new HdmiCecController({
   clientPath:   '/usr/osmc/bin/cec-client --type r --osd-name Kodi',
@@ -11,19 +12,22 @@ var controller = global.controller = new HdmiCecController({
   }
 })
 
-controller.on('message.error.format', (message) => {
-  console.warn("UNKNOW: " + message)
+controller.on('parse.error.format', (message) => {
+  // console.warn("ERR_FORMAT: " + message)
 })
-controller.on('message.error.parser', (msg) => {
-  console.warn("NOPARS: " + msg.raw)
+controller.on('parse.error.type', (msg) => {
+  // console.warn("ERR_TYPE  : " + msg.raw)
+})
+controller.on('parse.error.parser', (msg) => {
+  // console.warn("ERR_PARSER: " + msg.raw)
 })
 controller.on('message.parsed', (raw) => {
-  console.log("PARSED: " + raw.raw)
+  // console.log("PARSED     : " + raw.raw)
 })
 
 
 controller.on('device.new', (device) => {
-  console.info("New device: " + device.device.name)
+  console.info("New device: " + device.device.name + " (ID: " + device.device.id + ")")
 })
 controller.on('device.update.osdName', (val, device) => {
   console.info(device.device.name + " has name: " + val)
@@ -31,9 +35,17 @@ controller.on('device.update.osdName', (val, device) => {
 controller.on('devices.update.active', (device) => {
   console.info("Active device: " + device.device.name)
 })
+controller.on('update', (status) => {
+  console.info("Status:")
+  console.info( util.inspect(status, false, null) )
+})
 
 
 controller.connect()
+
+setTimeout(() => {
+  
+}, 1000)
 
 
 
